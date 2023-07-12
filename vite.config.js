c97ce30c2ea64e791dcb7384229bef669befeb50
Derrick Hammer <derrick@derrickhammer.com>
2023-07-12 04:46:26 -0400

    ci: add vite.config.js as an actual file

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,30 @@
-node_modules/presetter/generated/@lumeweb/kernel-discovery-irc/vite.config.js
+import { defineConfig } from "vite";
+import optimizer from "vite-plugin-optimizer";
+import inject from "@rollup/plugin-inject";
+
+export default defineConfig({
+  build: {
+    outDir: "lib",
+    lib: {
+      entry: "build/index.js",
+      name: "main",
+      formats: ["cjs"],
+      fileName: "index",
+    },
+    minify: false,
+    rollupOptions: { output: { inlineDynamicImports: true } },
+  },
+  resolve: {
+    dedupe: ["@lumeweb/libportal", "@lumeweb/libweb", "@lumeweb/libkernel"],
+    alias: { tls: "./build/net.js", net: "./build/net.js" },
+  },
+  plugins: [
+    optimizer({
+      "node-fetch":
+        "const e = undefined; export default e;export {e as Response, e as FormData, e as Blob};",
+    }),
+    inject({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+});
